374c9e42979d6257d840df7b4264a1bf4baddfe4
Rishi Gandhi <rishigandhi2002@gmail.com>
2023-10-18 21:19:58 -0230

    Update tweet.py

--- docs/tweet.py
+++ docs/tweet.py
@@ -1,26 +1,36 @@
-from flask import Flask, request, redirect, url_for, render_template
+import tkinter as tk
 
-app = Flask(__name__)
+def send_message():
+    username = username_entry.get()
+    message = message_entry.get()
+    if username.strip() != "" and message.strip() != "":
+        message_text = f"{username}: {message}"
+        chat_display.insert(tk.END, message_text + "\n")
+        username_entry.delete(0, tk.END)
+        message_entry.delete(0, tk.END)
 
-# A dictionary to simulate user accounts (username: password)
-users = {
-    "username1": "password1",
-    "username2": "password2"
-}
+app = tk.Tk()
+app.title("Twitter-like Chat")
 
-@app.route('/')
-def index():
-    return "Welcome to the Chat Forum!"
+chat_frame = tk.Frame(app)
+chat_frame.pack(padx=10, pady=10)
 
-@app.route('/chat/<username>', methods=['GET', 'POST'])
-def chat(username):
-    if request.method == 'POST':
-        message = request.form['message']
-        if message.strip() != "":
-            # Handle the message (you can save it to a database, etc.)
-            return redirect(url_for('chat', username=username))
-    
-    return render_template('chat.html', username=username)
+chat_display = tk.Text(chat_frame, height=10, width=40)
+chat_display.pack()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+username_label = tk.Label(chat_frame, text="Your Name:")
+username_label.pack()
+
+username_entry = tk.Entry(chat_frame)
+username_entry.pack()
+
+message_label = tk.Label(chat_frame, text="What's happening?")
+message_label.pack()
+
+message_entry = tk.Entry(chat_frame)
+message_entry.pack()
+
+send_button = tk.Button(chat_frame, text="Tweet", command=send_message)
+send_button.pack()
+
+app.mainloop()
